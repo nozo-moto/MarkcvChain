00cc77eb52ad0d2ce697d96ce42d3a15680cc828
Your Name <nozomotoitech@gmail.com>
2017-09-30 10:00:07 +0900

    fix to be a library

---
+++ MarkcvChain/__init__.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+import MarkcvChain
+ 
+__author__  = 'nozomi0966'
+__version__ = '0.0.1'
+__license__ = 'MIT'
---
+++ setup.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+from setuptools import setup, find_packages
+from pip_github_test import __author__, __version__, __license__
+ 
+setup(
+        name             = 'MarcvChain',
+        version          = __version__,
+        description      = 'Yahooの形態素解析とマルコフ連鎖を使用して、文章の自動生成をするライブラリ',
+        license          = __license__,
+        author           = __author__,
+        author_email     = '',
+        url              = 'https://github.com/nozomi0966/MarkcvChain',
+        keywords         = 'MarkcvChain',
+        packages         = find_packages(),
+        install_requires = [],
+        )
